364135e8433b7b82cda46248b802701f6459f456
John Dillick <john.dillick@highlights.com>
2014-03-20 10:25:33 -0400

    Added autoload static method example to api file.

--- register_hooks.api.php
+++ register_hooks.api.php
@@ -32,3 +32,13 @@ class Test2 {
     );
   }
 }
+
+/**
+ * If your module were using xautoload, by donquixote, this class would be autoloaded from
+ * my_module_name/lib/Drupal/my_module_name/MyClass.php whenever hook_views_data() was invoked, and
+ * the static method my_method() would be called.
+ */
+register_hooks_register('my_module_name', 'views_data', array(
+    'Drupal\my_module_name\MyClass', 'my_method'
+  )
+);
